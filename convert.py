fb592474ee8c44e1c30a94111f5964bbd86ead48
fun <goodboy@local.party>
2021-03-03 15:20:22 +0800

    update

--- convert.py
+++ convert.py
@@ -1,6 +1,5 @@
 # coding:utf-8
 import json
-import re
 import argparse
 def convert(domain,file):
     data=[]
@@ -8,12 +7,14 @@ def convert(domain,file):
     base={"name":"","value":"","domain":""}
     with open(file,'r+') as f:
         string=f.read()
-    string1=string+';'
-    key=re.findall(r"[\w\-\:\=]+(?==[\w\-\"\(])",string1)
-    values=re.findall(r"=(.*?);",string1)
-    for i in range(len(key)):
-        base["name"]=key[i]
-        base['value']=values[i]
+    string=string.strip('\n')
+    count=string.count(';')+1
+    string=string.split(';')
+    for i in range(count):
+        name=string[i].split('=')[0].lstrip()
+        value=string[i].split('=')[1].lstrip()
+        base['value']=value
+        base['name']=name
         base['domain']=domain
         data.append(base.copy())
     print(json.dumps(data))
@@ -22,7 +23,4 @@ if __name__=='__main__':
     parser.add_argument("-f",dest='file',help='set cookie file')
     parser.add_argument("-d",dest='domain',help='set Second-level domain; example:github.com')
     args=parser.parse_args()
-    try:
-        convert(args.domain,args.file)
-    except:
-        print("Error!! \nusage:python3 convert.py -d github.com -f cookie.txt\nPlease set Second-level domain")
+    convert(args.domain,args.file)
